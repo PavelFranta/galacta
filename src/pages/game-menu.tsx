1f9b33b2a9f407cd0e3f08da72c175fe1674b539
Pavel Franta <pavel.franta@zonky.cz>
2022-03-31 21:30:53 +0200

    Logo Galacta

--- src/pages/game-menu.tsx
+++ src/pages/game-menu.tsx
@@ -8,23 +8,26 @@ function GameMenu() {
   return (
     <div className="flex justify-center items-center h-full flex-col">
       {!displayGame && (
-        <div className="w-[150px] flex flex-col gap-8">
-          <button
-            className="border p-4 border-black"
-            onClick={shouldDisplayGame}
-          >
-            Nová hra
-          </button>
-          <button className="border p-4 border-black">Ladder</button>
-          <a
-            className="border p-4 border-black no-underline text-center"
-            href="https://www.youtube.com/watch?v=rMcEwaGz_64"
-          >
-            Konec
-          </a>
-        </div>
+        <>
+          <h1 className="text-9xl mb-28 animate-pulse">厶卂乚卂匚丅卂</h1>
+          <div className="w-[150px] flex flex-col gap-8">
+            <button
+              className="border p-4 border-black"
+              onClick={shouldDisplayGame}
+            >
+              Nová hra
+            </button>
+            <button className="border p-4 border-black">Ladder</button>
+            <a
+              className="border p-4 border-black no-underline text-center"
+              href="https://www.youtube.com/watch?v=rMcEwaGz_64"
+            >
+              Konec
+            </a>
+          </div>
+        </>
       )}
-      {displayGame && <div>hra zacina</div>}
+      {displayGame && <div className="text-3xl">hra zacina</div>}
     </div>
   )
 }
